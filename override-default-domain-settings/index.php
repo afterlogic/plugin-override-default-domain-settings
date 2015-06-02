431fe68564ec3f41ab7b9f7dbe85e9b45cc58b65
support@afterlogic.com <support@afterlogic.com>
2015-06-02 17:18:23 +0300

    add files

---
+++ override-default-domain-settings/index.php
@@ -0,0 +1,39 @@
+<?php
+
+/* -AFTERLOGIC LICENSE HEADER- */
+
+class_exists('CApi') or die();
+
+class COverrideDefaultDomainSettingsPlugin extends AApiPlugin
+{
+	/**
+	 * @param CApiPluginManager $oPluginManager
+	 */
+	public function __construct(CApiPluginManager $oPluginManager)
+	{
+		parent::__construct('1.0', $oPluginManager);
+
+		$this->AddHook('api-pre-create-account-process-call', 'PluginApiPreCreateAccountProcessCall');
+	}
+
+	/**
+	 * @param CAccount $oAccountToCreate
+	 */
+	public function PluginApiPreCreateAccountProcessCall(&$oAccountToCreate)
+	{
+		if ($oAccountToCreate instanceof CAccount && $oAccountToCreate->Domain && $oAccountToCreate->Domain->IsDefaultDomain)
+		{
+			$sEmailDomain = api_Utils::GetDomainFromEmail($oAccountToCreate->Email);
+			if (0 < strlen($sEmailDomain))
+			{
+				$oAccountToCreate->IncomingMailServer = 'mail.'.$sEmailDomain;
+				$oAccountToCreate->OutgoingMailServer = 'mail.'.$sEmailDomain;
+
+//				$oAccountToCreate->IncomingMailServer = 'imap.'.$sEmailDomain;
+//				$oAccountToCreate->OutgoingMailServer = 'smtp.'.$sEmailDomain;
+			}
+		}
+	}
+}
+
+return new COverrideDefaultDomainSettingsPlugin($this);
